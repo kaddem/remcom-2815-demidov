20aed8ff9c6642769eafcbebcf401dfc1c1c96c3
Kirill Demidov <kademidov@gmail.com>
2023-03-22 02:35:12 +0300

    аккордионы, табы, фильтрация

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -6,15 +6,62 @@ $(document).ready(function() {
   });
 
 
-  $('.j-main-menu a').on('click', function() {
-    console.log('clickkkkk');
+  // Аккордеоны 1
+  let prevAccordionBtn = undefined;
+
+  $('.j-accordion-btn').on('click', function() {
+
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+    } else {
+      $(prevAccordionBtn).next().slideUp();
+      $(this).next().slideDown();
+      prevAccordionBtn = this;
+    }
+
   });
 
-  let textLogo = $('.header-logo').html('<u>UUURRRR</u><script>alert("гони бабки! Твой комп заражен!");</script>');
 
-  console.log(textLogo);
+  // Табы
+  $('.j-contacts-link').on('click', function() {
+    let index = $(this).index('.j-contacts-link');
 
-  console.log($('.j-main-menu'));
+    $('.j-contacts-link').removeClass('active');
+    // $('.j-contacts-link').eq(index).addClass('active');
+    // Аналог:
+    $(this).addClass('active');
+
+    $('.j-contacts-item').removeClass('active');
+    $('.j-contacts-item').eq(index).addClass('active');
+
+  });
+
+
+  // Фильтр в наших работах
+  $('.j-works-link').on('click', function() {
+    $('.j-works-link').removeClass('active');
+    $(this).addClass('active');
+
+    let dataFilter = $(this).data('filter');
+
+    if (dataFilter === 'all') {
+      $('.j-works-item').show();
+      return;
+    }
+
+    // Внутри each - свой собственный this
+    $('.j-works-item').each(function() {
+      let dataType = $(this).data('type');
+
+      if (dataFilter === dataType) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    })
+
+  });
+
 
 });
 
